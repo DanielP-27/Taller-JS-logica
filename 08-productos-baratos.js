7a2bb10d7ea4605fe51f0247fb850d9a6d7c42c8
Daniel Pineda <angeldpb27@gmail.com>
2023-07-06 09:22:10 -0500

    [/] se incluye descripcion en ingles del ejercicio No. 08

--- 08-productos-baratos.js
+++ 08-productos-baratos.js
@@ -1,6 +1,9 @@
 // Escribe una función llamada productosBaratos que reciba un arreglo de objetos (que representan productos) y retorne un nuevo arreglo con los nombres de los productos cuyo precio esté entre 5 y 10:
 // Nota: No olvide usar console.log() para mostrar los resultados retornados por la función productosBaratos:
 
+// Write a function called "productosBaratos" that receives an array of objects (that represents products) and returns a new array with the numbers of the products with a value range between 5 and 10: 
+// Note: do not forget to use console.log() to show the results returned for the "productosBaratos" function. 
+
 let prods = [
   { nombre: "Arroz", precio: 5 },
   { nombre: "Pan", precio: 3 },
